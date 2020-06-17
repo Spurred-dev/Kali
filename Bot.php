c3ab36a81df9dc3ba115eaffdf8ae4fe7917b427
Spurred-dev <62203581+Spurred-dev@users.noreply.github.com>
2020-06-17 17:54:28 +0430

    Update Bot.php

--- Bot.php
+++ Bot.php
@@ -6,18 +6,8 @@ if (!file_exists('madeline.php')) {
 }
 define('MADELINE_BRANCH', 'deprecated');
 include 'madeline.php';
-function bot($method, $datas = []) {
-	$token = file_get_contents("_token.txt");
-	$url = "https://api.telegram.org/bot$token/" . $method;
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, $url);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $datas);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-	$res = curl_exec($ch);
-	curl_close($ch);
-	return json_decode($res, true);
-}
+
+
 function Channel(){
     
 $settings['app_info']['api_id'] = 579315;
@@ -25,25 +15,27 @@ $settings['app_info']['api_hash'] = '4ace69ed2f78cec268dc7483fd3d3424';
 $MadelineProto = new \danog\MadelineProto\API('mdddd.madeline', $settings);
 $MadelineProto->start();
 
-bot('sendMessage', ['chat_id' => file_get_contents("_group.txt"), 'text' => "checker start run | type - channel",]);
+$MadelineProto->messages->sendMessage(['peer' => file_get_contents("_ad.txt"), 'message' => "/send checker start run | type - channel"]);
 
 
 $updates = $MadelineProto->channels->createChannel(['broadcast' => true,'megagroup' => false,'title' => file_get_contents("_name.txt"), ]);
 $chat_mack = $updates['updates'][1];
-
+$i = 0;
 while(1){
     $users = explode("\n",file_get_contents("_users.txt"));
     foreach($users as $user){
         if($user != ""){
             try{
             	$MadelineProto->messages->getPeerDialogs(['peers' => [$user]]);
+            	echo "Loop : ".$i++." | Time : ".date("s")." | User : $user\n";
             } catch (\danog\MadelineProto\Exception | \danog\MadelineProto\RPCErrorException $e) {
                     try{
+                        echo "Done user @$user";
                         $MadelineProto->channels->updateUsername(['channel' => $chat_mack, 'username' => $user]);
                         
                         $ch = file_get_contents("_ch.txt");
-				        $Text = "- NewUsername: @$user";
-                        bot('sendMessage', ['chat_id' => file_get_contents("_group.txt"), 'text' => $Text]);
+				        $Text = "❗️| Hi HaDi.\n————————————\n⚠️| New Update : @$user !\n❗️| Channel Checker : @ !\n❕| Loop's : $i\n————————————\n❗️| By : @iT_HaDi";
+                        $MadelineProto->messages->sendMessage(['peer' => file_get_contents("_ad.txt"), 'message' => "/send ".$Text]);
                         $MadelineProto->messages->sendMessage(['peer' => $chat_mack, 'message' => $Text]);
                         $data = str_replace("\n".$user,"", file_get_contents("_users.txt"));
                         file_put_contents("_users.txt", $data);
@@ -54,23 +46,22 @@ while(1){
                         if($e->getMessage() == "The provided username is not valid"){
                             $data = str_replace("\n".$user,"", file_get_contents("_users.txt"));
                             file_put_contents("_users.txt", $data);
-                            bot('sendMessage', ['chat_id' => file_get_contents("_group.txt"), 'text' => 'This username is (banned/smaller than 5) i delete it from users list : @$user ',]);
+                            $MadelineProto->messages->sendMessage(['peer' => file_get_contents("_ad.txt"), 'message' => '/send This username is (banned/smaller than 5) i delete it from users list : @' . $user ]);
                         }elseif(preg_match('/FLOOD_WAIT_(.*)/i', $e->getMessage())){
                             $seconds = str_replace('FLOOD_WAIT_', '', $e->getMessage());
-                            bot('sendMessage', ['chat_id' => file_get_contents("_group.txt"), 'text' => ' I m sleeping : $seconds ',]);
+                            $MadelineProto->messages->sendMessage(['peer'=>file_get_contents("_ad.txt"),'message'=>"/send I'm sleeping : ".$seconds]);
                             sleep($seconds);
                         }elseif($e->getMessage() == "USERNAME_OCCUPIED"){
                             $data = str_replace("\n".$user,"", file_get_contents("_users.txt"));
                             file_put_contents("_users.txt", $data);
-                            bot('sendMessage', ['chat_id' => file_get_contents("_group.txt"), 'text' => " could not save it : @$user",]);
+                            $MadelineProto->messages->sendMessage(['peer'=>file_get_contents("_ad.txt"),'message'=>"/send I could not save it : @$user"]);
                         }elseif($e->getMessage() == "CHANNELS_ADMIN_PUBLIC_TOO_MUCH"){
                              $data = str_replace("\n".$user,"", file_get_contents("_users.txt"));
                             file_put_contents("_users.txt", $data);
-                            bot('sendMessage', ['chat_id' => file_get_contents("_group.txt"), 'text' => "CHANNELS_ADMIN_PUBLIC_TOO_MUCH : @$user",]);
+                            $MadelineProto->messages->sendMessage(['peer'=>file_get_contents("_ad.txt"),'message'=>"/send CHANNELS_ADMIN_PUBLIC_TOO_MUCH : @$user"]);
                           
                         }else{
-                            bot('sendMessage', ['chat_id' => file_get_contents("_group.txt"), 'text' =>  "ERROR - ".$e->getMessage()
-]);
+                            $MadelineProto->messages->sendMessage(['peer' => file_get_contents("_ad.txt"), 'message' => "ERROR - ".$e->getMessage()]);
                         }
 
   
@@ -87,44 +78,39 @@ $settings['app_info']['api_hash'] = '4ace69ed2f78cec268dc7483fd3d3424';
 $MadelineProto = new \danog\MadelineProto\API('mdddd.madeline', $settings);
 $MadelineProto->start();
 
-bot('sendMessage', ['chat_id' => file_get_contents("_group.txt"), 'text' => " checker start run | type - account",]);
-
+$MadelineProto->messages->sendMessage(['peer' => file_get_contents("_ad.txt"), 'message' => "/send checker start run | type - account"]);
+$i = 0;
 while(1){
     $users = explode("\n",file_get_contents("_users.txt"));
     foreach($users as $user){
         if($user != ""){
             try{
             	$MadelineProto->messages->getPeerDialogs(['peers' => [$user], ]);
+            	echo "Loop : ".$i++." | Time : ".date("s")." | User : $user\n";
             } catch (\danog\MadelineProto\Exception | \danog\MadelineProto\RPCErrorException $e) {
                     try{
+                        echo "Done user @$user";
                         $MadelineProto->account->updateUsername(['username'=>$user]);
                         $ch = file_get_contents("_ch.txt");
-				        $Text = "- NewUsername: @$user";
-                        bot('sendMessage', ['chat_id' => file_get_contents("_group.txt"), 'text' =>$Text]);
+				        $Text = "❗️| Hi HaDi.\n————————————\n⚠️| New Update : @$user !\n❗️| Channel Checker : @ !\n❕| Loop's : $i\n————————————\n❗️| By : @iT_HaDi";
+                        $MadelineProto->messages->sendMessage(['peer' => file_get_contents("_ad.txt"), 'message' => "/send ".$Text]);
                         $data = str_replace("\n".$user,"", file_get_contents("_users.txt"));
                         file_put_contents("_users.txt", $data);
-                            }catch(Exception $e){
-                        echo $e->getMessage();
+                    }catch(Exception $e){
                         if($e->getMessage() == "The provided username is not valid"){
                             $data = str_replace("\n".$user,"", file_get_contents("_users.txt"));
                             file_put_contents("_users.txt", $data);
-                            bot('sendMessage', ['chat_id' => file_get_contents("_group.txt"), 'text' => 'This username is (banned/smaller than 5) i delete it from users list : @$user ',]);
+                            $MadelineProto->messages->sendMessage(['peer' => file_get_contents("_ad.txt"), 'message' => '/send This username is (banned/smaller than 5) i delete it from users list : @' . $user ]);
                         }elseif(preg_match('/FLOOD_WAIT_(.*)/i', $e->getMessage())){
                             $seconds = str_replace('FLOOD_WAIT_', '', $e->getMessage());
-                            bot('sendMessage', ['chat_id' => file_get_contents("_group.txt"), 'text' => ' I m sleeping : $seconds ',]);
+                            $MadelineProto->messages->sendMessage(['peer'=>file_get_contents("_ad.txt"),'message'=>"/send I'm sleeping : ".$seconds]);
                             sleep($seconds);
                         }elseif($e->getMessage() == "USERNAME_OCCUPIED"){
                             $data = str_replace("\n".$user,"", file_get_contents("_users.txt"));
                             file_put_contents("_users.txt", $data);
-                            bot('sendMessage', ['chat_id' => file_get_contents("_group.txt"), 'text' => " could not save it : @$user",]);
-                        }elseif($e->getMessage() == "CHANNELS_ADMIN_PUBLIC_TOO_MUCH"){
-                             $data = str_replace("\n".$user,"", file_get_contents("_users.txt"));
-                            file_put_contents("_users.txt", $data);
-                            bot('sendMessage', ['chat_id' => file_get_contents("_group.txt"), 'text' => "CHANNELS_ADMIN_PUBLIC_TOO_MUCH : @$user",]);
-                          
+                            $MadelineProto->messages->sendMessage(['peer'=>file_get_contents("_ad.txt"),'message'=>"/send I could not save it : @$user"]);
                         }else{
-                            bot('sendMessage', ['chat_id' => file_get_contents("_group.txt"), 'text' =>  "ERROR - ".$e->getMessage()
-]);
+                            $MadelineProto->messages->sendMessage(['peer' => file_get_contents("_ad.txt"), 'message' => "ERROR - ".$e->getMessage()]);
                         }
 
   
